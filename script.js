1f328e911579df856ab08ff01adf5ee1defa641a
Firengiz77 <81989576+Firengiz77@users.noreply.github.com>
2021-04-06 14:54:07 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+
+    $(".owl-carousel").owlCarousel({
+        items: 1,
+        loop:true,
+        autoplay:true,
+        nav:true,
+       
+
+    });
+  
+  
+  
+   });
